3e3ad7587a9055d769ada03a2e94bc5fdaff4b26
Tabish Nanhekhan <1631995@gmail.com>
2023-02-01 12:04:14 +0100

    word crud

--- controllers/word-controller.js
+++ controllers/word-controller.js
@@ -20,19 +20,29 @@ exports.handleUpdate = (req, res, next) => {
     const word = req.body.word;
     const image = req.file;
 
-    wordRepository.updateWord(req.params.wordId, word, image)
-        .then((response) => {
-            res.redirect('/cms/words');
-        });
+    if (req.body.delete) {
+        wordRepository.deleteWord(req.params.wordId)
+            .then((response) => {
+                res.redirect('/cms/words');
+            });
+    } else {
+        wordRepository.updateWord(req.params.wordId, word, image)
+            .then((response) => {
+                res.redirect('/cms/words');
+            });
+    }
 }
 
 exports.getDetailUpdateView = (req, res, next) => {
     wordRepository.getWord(req.params.wordId)
         .then((response) => {
-            res.render('cms/words/detail/index', {title: 'Update Word: ' + response.word,
+            res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+            res.render('cms/words/detail/index', {
+                title: 'Update Word: ' + response.word,
                 editable: true,
                 word: response.word,
                 imageUrl: response.image,
-                wordlists: response.wordlists});
+                wordlists: response.wordlists
+            });
         });
 }
--- data/word-repository.js
+++ data/word-repository.js
@@ -1,4 +1,4 @@
-const {database, storage, storage_bucket_id, database_id, word_collection_id} = require ("../service/appwrite");
+const {database, storage, storage_bucket_id, database_id, word_collection_id} = require("../service/appwrite");
 const uuid = require("uuid");
 const {InputFile} = require("node-appwrite");
 const {Readable} = require("stream");
@@ -16,14 +16,15 @@ exports.getWord = (word_id) => {
 }
 
 exports.uploadWord = (word, image) => {
+    const new_id = uuid.v4();
     if (image) {
-        return createWordWithImage(word, image)
+        return createWordWithImage(word, image, new_id)
     } else {
-        return createWordWithoutImage(word, image)
+        return createWordWithoutImage(word, new_id)
     }
 }
-function createWordWithoutImage(word, image) {
-    const id = uuid.v4();
+
+function createWordWithoutImage(word, id) {
     return new Promise((resolve, reject) => {
         database.createDocument(database_id,
             word_collection_id,
@@ -40,8 +41,7 @@ function createWordWithoutImage(word, image) {
     });
 }
 
-function createWordWithImage(word, image) {
-    const id = uuid.v4();
+function createWordWithImage(word, image, id) {
     return new Promise((resolve, reject) => {
         storage.createFile(storage_bucket_id,
             id,
@@ -72,44 +72,47 @@ function createWordWithImage(word, image) {
 }
 
 exports.updateWord = (id, word, image) => {
-    console.log(word);
-
     if (image) {
-
+        return updateWordWithImage(id, word, image)
     } else {
-        return new Promise((resolve, reject) => {
-            deleteWord(id)
-                .then((response) => {
-                    storage.updateFile(storage_bucket_id,
-                        id)
-                        .then((storage_response) => {
-                            const image_url_string = `${process.env.AW_ENDPOINT}/storage/buckets/${storage_bucket_id}/files/${id}/view?project=${process.env.AW_PROJECT_ID}`
-                            database.createDocument(database_id,
-                                word_collection_id,
-                                id,
-                                {
-                                    word: word,
-                                    image: image_url_string
-                                })
-                                .then((response) => {
-                                    resolve(response);
-                                })
-                                .catch((error) => {
-                                    console.log("db error", error)
-                                    reject(error);
-                                });
-                        })
-                        .catch((storage_error) => {
-                            console.log("storage error", storage_error)
-                            reject(storage_error);
-                        });
-
-                })
-                .catch((error) => {
-                    reject(error);
-                });
-        });
+        return updateWordWithoutImage(id, word)
     }
+}
+
+function updateWordWithImage(id, word, image) {
+    return new Promise((resolve, reject) => {
+        deleteWord(id)
+            .then((response) => {
+                createWordWithImage(word, image, id)
+                    .then((response) => {
+                        resolve(response);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+function updateWordWithoutImage(word_id, word) {
+    return new Promise((resolve, reject) => {
+        database.deleteDocument(database_id, word_collection_id, word_id)
+            .then((response) => {
+                createWordWithoutImage(word, word_id)
+                    .then((response) => {
+                        resolve(response);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
 }
 
 exports.deleteWord = (word_id) => {
@@ -129,8 +132,18 @@ function deleteWord(word_id) {
                     });
             })
             .catch((error) => {
-                console.log("delete error", error);
-                reject(error);
+                // No image to delete
+                if (error.code === 404) {
+                    database.deleteDocument(database_id, word_collection_id, word_id)
+                        .then((response) => {
+                            resolve(response);
+                        })
+                        .catch((error) => {
+                            reject(error);
+                        });
+                } else {
+                    reject(error);
+                }
             });
     });
 }
