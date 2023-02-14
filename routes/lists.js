caf779af144917a27847001790763e4122ea4431
Tabish Nanhekhan <1631995@gmail.com>
2023-02-14 19:05:29 +0100

    add await

--- routes/lists.js
+++ routes/lists.js
@@ -5,19 +5,19 @@ const upload = multer()
 const listsController = require("../controllers/list-controller");
 
 router.get('/upload', async (req, res, next) => {
-    listsController.getDetailCreateView(req, res, next);
+    await listsController.getDetailCreateView(req, res, next);
 });
 
 router.post('/upload', upload.single('list'), async (req, res, next) => {
-    listsController.handleCreate(req, res, next);
+   await listsController.handleCreate(req, res, next);
 });
 
 router.get('/:listId', async (req, res, next) => {
-    listsController.getDetailUpdateView(req, res, next)
+    await listsController.getDetailUpdateView(req, res, next)
 });
 
 router.post('/:listId', upload.single('list'), async (req, res, next) => {
-    listsController.handleUpdate(req, res, next)
+    await listsController.handleUpdate(req, res, next)
 });
 
 module.exports = router;
