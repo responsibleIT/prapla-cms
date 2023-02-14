5e027ada57dfca803379052653b84fa4d790419d
Tabish Nanhekhan <1631995@gmail.com>
2023-02-15 00:52:54 +0100

    session test

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -7,20 +7,24 @@ exports.getLoginView = (req, res, next) => {
 exports.validateLogin = (req, res, next) => {
     let session_id = req.body["session_id"];
     let user_id = req.body["user_id"];
-    if (session_id && user_id) {
-        appwrite.isValidSession(user_id, session_id)
-            .then((response) => {
-                res.cookie("user_id", user_id, {maxAge: 2 * 60 * 60 * 1000, httpOnly: true});
-                res.cookie("session_id", session_id, {maxAge: 2 * 60 * 60 * 1000, httpOnly: true});
-                res.redirect('/cms');
-            })
-            .catch((error) => {
-                console.log(error);
-                res.redirect('/');
-            });
-    } else {
-        res.redirect('/');
-    }
+    // if (session_id && user_id) {
+    //     appwrite.isValidSession(user_id, session_id)
+    //         .then((response) => {
+    //             res.cookie("user_id", user_id, {maxAge: 2 * 60 * 60 * 1000, httpOnly: true});
+    //             res.cookie("session_id", session_id, {maxAge: 2 * 60 * 60 * 1000, httpOnly: true});
+    //             res.redirect('/cms');
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //             res.redirect('/');
+    //         });
+    // } else {
+    //     res.redirect('/');
+    // }
+    
+    res.cookie("user_id", user_id, {maxAge: 2 * 60 * 60 * 1000, httpOnly: true});
+    res.cookie("session_id", session_id, {maxAge: 2 * 60 * 60 * 1000, httpOnly: true});
+    res.redirect('/cms');
 }
 
 exports.verifyCookie = (req, res, next) => {
