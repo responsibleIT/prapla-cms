1803b06003f500a74f037b55a62225804e49fa60
Tabish <1631995@gmail.com>
2023-02-22 10:20:17 +0100

    ui changes + update api get

--- data/api-repository.js
+++ data/api-repository.js
@@ -1,18 +1,22 @@
 const spellRepo = require("./spell-repository");
 const wordRepo = require("./word-repository");
+const studentRepo = require("./student-repository");
 
 exports.getWholeListBySpell = (spell) => {
     return new Promise(async (resolve, reject) => {
-        try{
+        try {
             let spellDocument = await spellRepo.getSpell(spell);
             let wordlist_id = spellDocument.wordlist;
             let allWords = await wordRepo.getWords();
+            let student = await studentRepo.getStudent(spellDocument.student);
 
             let wordList = allWords.map(word => {
                 if (word.subscribedWordList.includes(wordlist_id)) {
                     return {id: word.id, word: word.word, image: word.image}
                 }
             }).filter(Boolean);
+
+            wordList.unshift({id: spellDocument.student, student: student.name});
             resolve(wordList);
         } catch (error) {
             reject(error);
