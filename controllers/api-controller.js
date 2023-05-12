aac92d8b6cbbc6a3a0786ae7d53ddb14656533d2
Tabish <1631995@gmail.com>
2023-05-12 15:55:08 +0200

    created second part of quiz

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -1,10 +1,45 @@
 const apiRepo = require('../data/api-repository');
 
 exports.getWordList = async (req, res) => {
-    try{
+    try {
         const wordList = await apiRepo.getWholeListBySpell(req.query.spell);
-        res.status(200).json(wordList);
+        const obj = [wordList, createPartTwo(wordList)];
+
+        res.status(200).json(obj);
     } catch (error) {
         res.status(404).json({message: "Not found"});
     }
 };
+
+function createPartTwo(wordList) {
+    let partTwo = [];
+    let no_of_questions = Math.floor(Math.random() * 5) + 1;
+    for (let i = 0; i < no_of_questions; i++) {
+        let sample = getRandom(wordList, 4);
+        sample.map((word) => {
+            word.correct = false;
+        });
+
+        let randomNr = Math.floor(Math.random() * sample.length);
+        sample[randomNr].correct = true;
+
+        partTwo.push(sample);
+    }
+    return partTwo;
+}
+
+function getRandom(arr, n) {
+    arr.shift();
+
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
+}
